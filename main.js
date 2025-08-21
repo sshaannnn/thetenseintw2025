faeb7a54b389f6f31650c9deea3d1014ea6a1bb9
sshaannnn <shan87826@gmail.com>
2025-08-21 17:33:55 +0800

    feat: axious

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const A7 = document.getElementById('A7');
-
-  A7.addEventListener('click', () => {
-    console.log(123);
+document.querySelectorAll('svg [data-seat]').forEach((el) => {
+  el.addEventListener('click', () => {
+    fetchData(el.dataset.seat);
+    const seatNum = el.dataset.seat;
+    console.log(seatNum);
   });
 });
+
+async function fetchData(seat) {
+  try {
+    console.log('seat', seat);
+    const res = await axios.get('https://fakestoreapi.com/products');
+    const products = res?.data;
+    let str = '';
+    products.forEach((x, i) => {
+      str += ` <tr>
+                <th scope="row">${i + 1}</th>
+                <td>${x?.category}</td>
+                <td>${x?.price}</td>
+              </tr>`;
+    });
+
+    const list = document.querySelector('.list');
+    list.innerHTML = str;
+  } catch (error) {
+    console.error('Uh-oh! Error fetching data:', error.message);
+  }
+}
