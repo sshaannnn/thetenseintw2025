286d3b145f8068663e49816280b299e95261e5db
sshaannnn <shan87826@gmail.com>
2025-08-21 19:33:10 +0800

    feat: image link

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
+let preSeatNum = '';
+
 document.querySelectorAll('svg [data-seat]').forEach((el) => {
   el.addEventListener('click', () => {
-    fetchData(el.dataset.seat);
     const seatNum = el.dataset.seat;
-    console.log(seatNum);
+    if (preSeatNum === seatNum) return;
+    fetchData(seatNum);
+    preSeatNum = seatNum;
   });
 });
 
 async function fetchData(seat) {
   try {
-    console.log('seat', seat);
     const res = await axios.get('https://fakestoreapi.com/products');
     const products = res?.data;
     let str = '';
+    console.log(products);
     products.forEach((x, i) => {
       str += ` <tr>
-                <th scope="row">${i + 1}</th>
-                <td>${x?.category}</td>
-                <td>${x?.price}</td>
+            <th>
+              <a href="${x?.image}" class="hand" target="_blank">
+                <img src="image.png">
+              </a>
+            </th>
+                <td>B區 109 22排</td>
+                <td>shan826</td>
               </tr>`;
     });
-
+    console.log(333333);
     const list = document.querySelector('.list');
     list.innerHTML = str;
   } catch (error) {
